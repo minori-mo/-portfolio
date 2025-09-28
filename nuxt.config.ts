cda9e3bb9d535a0084adc24a18a68a1240b5d667
minori_motoda <minori.motoda1993@gmail.com>
2025-09-28 12:33:26 +0900

    設定ファイルの変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
+  ssr: true,
+  nitro: {
+    preset: "netlify-static", // 🔑 Netlify向けの静的出力
+  },
   css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
